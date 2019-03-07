abacf18c9372d7d3b701acd314d6296ad452e966
lzflzflzflzf <1316745258@qq.com>
2019-03-07 14:57:46 +0800

    zefa

--- src/main/java/com/aiwu/controller/RentController.java
+++ src/main/java/com/aiwu/controller/RentController.java
@@ -61,6 +61,7 @@ public class RentController {
         System.out.println("=========================");
         System.out.println(str);
         System.out.println("=========================");
+        System.out.println("212");
         return list;
     }
 
